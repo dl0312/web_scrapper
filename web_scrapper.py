0638849502c31783c119348585257e57ccb97e37
Geon Lee <leegun2003@gmail.com>
2019-11-24 18:26:14 +0900

    🙇‍♂️ Extracting Indeed Pages 2

--- web_scrapper.py
+++ web_scrapper.py
@@ -5,11 +5,11 @@ indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class": "pagination"})
-pages = pagination.find_all('a')
-spans = []
 
-for page in pages:
-    spans.append(page.find("span"))
+links = pagination.find_all('a')
+pages = []
 
-print(spans)
+for link in links[:-1]:
+    pages.append(int(link.string))
 
+max_page = pages[-1]
