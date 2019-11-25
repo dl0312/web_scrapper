2d887aa310f02799011e81b81e723efae439b49d
Geon Lee <leegun2003@gmail.com>
2019-11-25 23:46:22 +0900

    🙇‍♂️Extracting Locations and Finishing up

--- indeed.py
+++ indeed.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
+
 def extract_indeed_pages():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
@@ -20,20 +21,30 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_job(html):
+    title = html.find("div", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class", "company"})
+    if company is not None:
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = str(company.find("a").string)
+        else:
+            company = str(company.string)
+        company = company.strip()
+    location = html.find("div", {"class", "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, 'link': f"https//www.indeed.com/view?jk={job_id}"}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page * LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
-            title = result.find("div", {"class": "title"}).find("a")["title"]
-            company = result.find("span", {"class", "company"}).find("a")
-            company_anchor = company.find("a")
-            if company_anchor is not None:
-                company = str(company.find("a").string)
-            else:
-                company = str(company.string)
-            company = company.strip()
-            print(company)
+            job = extract_job(result)
+            jobs.append(job)
+            # print(job)
     return jobs
