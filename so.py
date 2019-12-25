859a3ef1ada52f6e8476d0c98c984f421191283d
Geon Lee <leegun2003@gmail.com>
2019-12-25 23:17:43 +0900

    👨‍💻 StackOverflow extract_job

--- so.py
+++ so.py
@@ -10,6 +10,12 @@ def get_last_page():
     last_page = pages[-2].get_text(strip=True)
     return int(last_page)
 
+def extract_job(html):
+    title = html.find("h2").get_text()
+    company = html.find("h3").find("span").get_text(strip=True).strip("-")
+    location = html.find("h3").find("span", {"class": "fc-black-500"}).get_text(strip=True)
+    print({'title': title, 'company': company, 'location': location})
+
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
@@ -17,7 +23,9 @@ def extract_jobs(last_page):
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
-            print(result["data-jobid"])
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 def get_jobs():
     last_page = get_last_page()
