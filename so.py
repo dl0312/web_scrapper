12b92d68e1882cf443cb27e735a537e345993cd3
Geon Lee <leegun2003@gmail.com>
2019-12-26 23:05:27 +0900

    👨‍💻‍ StackOverflow Finish

--- so.py
+++ so.py
@@ -13,12 +13,14 @@ def get_last_page():
 def extract_job(html):
     title = html.find("h2").get_text()
     company = html.find("h3").find("span").get_text(strip=True).strip("-")
-    location = html.find("h3").find("span", {"class": "fc-black-500"}).get_text(strip=True)
-    print({'title': title, 'company': company, 'location': location})
+    location = html.find("h3").find("span", {"class": "fc-black-500"}).get_text(strip=True).strip("-").strip(" \r").strip("\n")
+    job_id = html['data-jobid']
+    return {'title': title, 'company': company, 'location': location, "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO: Page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
